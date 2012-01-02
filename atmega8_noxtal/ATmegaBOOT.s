851716450537b52bddfa47ae0afc0ac012c0393d
Bob <bob-nospam-@xbmc.org>
2012-01-03 00:41:44 +0100

    added: bootloader from http://todbot.com/blog/2009/05/26/minimal-arduino-with-atmega8/

---
+++ atmega8_noxtal/ATmegaBOOT.s
@@ -0,0 +1,947 @@
+	.file	"ATmegaBOOT.c"
+__SREG__ = 0x3f
+__SP_H__ = 0x3e
+__SP_L__ = 0x3d
+__CCP__ = 0x34
+__tmp_reg__ = 0
+__zero_reg__ = 1
+	.stabs	"/home/bob/Projects/bitlicht/atmega8_noxtal/",100,0,2,.Ltext0
+	.stabs	"ATmegaBOOT.c",100,0,2,.Ltext0
+	.text
+.Ltext0:
+	.stabs	"gcc2_compiled.",60,0,0,0
+	.stabs	"int:t(0,1)=r(0,1);-32768;32767;",128,0,0,0
+	.stabs	"char:t(0,2)=r(0,2);0;127;",128,0,0,0
+	.stabs	"long int:t(0,3)=@s32;r(0,3);020000000000;017777777777;",128,0,0,0
+	.stabs	"unsigned int:t(0,4)=r(0,4);0;0177777;",128,0,0,0
+	.stabs	"long unsigned int:t(0,5)=@s32;r(0,5);0;037777777777;",128,0,0,0
+	.stabs	"long long int:t(0,6)=@s64;r(0,6);01000000000000000000000;0777777777777777777777;",128,0,0,0
+	.stabs	"long long unsigned int:t(0,7)=@s64;r(0,7);0;01777777777777777777777;",128,0,0,0
+	.stabs	"short int:t(0,8)=r(0,8);-32768;32767;",128,0,0,0
+	.stabs	"short unsigned int:t(0,9)=r(0,9);0;0177777;",128,0,0,0
+	.stabs	"signed char:t(0,10)=@s8;r(0,10);-128;127;",128,0,0,0
+	.stabs	"unsigned char:t(0,11)=@s8;r(0,11);0;255;",128,0,0,0
+	.stabs	"float:t(0,12)=r(0,1);4;0;",128,0,0,0
+	.stabs	"double:t(0,13)=r(0,1);4;0;",128,0,0,0
+	.stabs	"long double:t(0,14)=r(0,1);4;0;",128,0,0,0
+	.stabs	"void:t(0,15)=(0,15)",128,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/inttypes.h",130,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/include/stdint.h",130,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/stdint.h",130,0,0,0
+	.stabs	"int8_t:t(3,1)=(0,10)",128,0,121,0
+	.stabs	"uint8_t:t(3,2)=(0,11)",128,0,122,0
+	.stabs	"int16_t:t(3,3)=(0,1)",128,0,123,0
+	.stabs	"uint16_t:t(3,4)=(0,4)",128,0,124,0
+	.stabs	"int32_t:t(3,5)=(0,3)",128,0,125,0
+	.stabs	"uint32_t:t(3,6)=(0,5)",128,0,126,0
+	.stabs	"int64_t:t(3,7)=(0,6)",128,0,128,0
+	.stabs	"uint64_t:t(3,8)=(0,7)",128,0,129,0
+	.stabs	"intptr_t:t(3,9)=(3,3)",128,0,142,0
+	.stabs	"uintptr_t:t(3,10)=(3,4)",128,0,147,0
+	.stabs	"int_least8_t:t(3,11)=(3,1)",128,0,159,0
+	.stabs	"uint_least8_t:t(3,12)=(3,2)",128,0,164,0
+	.stabs	"int_least16_t:t(3,13)=(3,3)",128,0,169,0
+	.stabs	"uint_least16_t:t(3,14)=(3,4)",128,0,174,0
+	.stabs	"int_least32_t:t(3,15)=(3,5)",128,0,179,0
+	.stabs	"uint_least32_t:t(3,16)=(3,6)",128,0,184,0
+	.stabs	"int_least64_t:t(3,17)=(3,7)",128,0,192,0
+	.stabs	"uint_least64_t:t(3,18)=(3,8)",128,0,199,0
+	.stabs	"int_fast8_t:t(3,19)=(3,1)",128,0,213,0
+	.stabs	"uint_fast8_t:t(3,20)=(3,2)",128,0,218,0
+	.stabs	"int_fast16_t:t(3,21)=(3,3)",128,0,223,0
+	.stabs	"uint_fast16_t:t(3,22)=(3,4)",128,0,228,0
+	.stabs	"int_fast32_t:t(3,23)=(3,5)",128,0,233,0
+	.stabs	"uint_fast32_t:t(3,24)=(3,6)",128,0,238,0
+	.stabs	"int_fast64_t:t(3,25)=(3,7)",128,0,246,0
+	.stabs	"uint_fast64_t:t(3,26)=(3,8)",128,0,253,0
+	.stabs	"intmax_t:t(3,27)=(3,7)",128,0,273,0
+	.stabs	"uintmax_t:t(3,28)=(3,8)",128,0,278,0
+	.stabn	162,0,0,0
+	.stabn	162,0,0,0
+	.stabs	"int_farptr_t:t(1,1)=(3,5)",128,0,77,0
+	.stabs	"uint_farptr_t:t(1,2)=(3,6)",128,0,81,0
+	.stabn	162,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/io.h",130,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/fuse.h",130,0,0,0
+	.stabs	"__fuse_t:t(5,1)=(5,2)=s2low:(0,11),0,8;high:(0,11),8,8;;",128,0,252,0
+	.stabn	162,0,0,0
+	.stabn	162,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/pgmspace.h",130,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/include/stddef.h",130,0,0,0
+	.stabs	"size_t:t(7,1)=(0,4)",128,0,211,0
+	.stabn	162,0,0,0
+	.stabs	"prog_void:t(6,1)=(6,2)=(6,2)",128,0,217,0
+	.stabs	"prog_char:t(6,3)=(6,4)=@s8;r(6,4);-128;127;",128,0,218,0
+	.stabs	"prog_uchar:t(6,5)=(6,6)=@s8;r(6,6);0;255;",128,0,219,0
+	.stabs	"prog_int8_t:t(6,7)=(6,8)=(0,10)",128,0,221,0
+	.stabs	"prog_uint8_t:t(6,9)=(6,10)=(0,11)",128,0,222,0
+	.stabs	"prog_int16_t:t(6,11)=(6,12)=(0,1)",128,0,223,0
+	.stabs	"prog_uint16_t:t(6,13)=(6,14)=(0,4)",128,0,224,0
+	.stabs	"prog_int32_t:t(6,15)=(6,16)=(0,3)",128,0,225,0
+	.stabs	"prog_uint32_t:t(6,17)=(6,18)=(0,5)",128,0,226,0
+	.stabs	"prog_int64_t:t(6,19)=(6,20)=(0,6)",128,0,228,0
+	.stabs	"prog_uint64_t:t(6,21)=(6,22)=(0,7)",128,0,229,0
+	.stabn	162,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/eeprom.h",130,0,0,0
+	.stabs	"/usr/lib/gcc/avr/4.5.3/include/stddef.h",130,0,0,0
+	.stabs	"ptrdiff_t:t(9,1)=(0,1)",128,0,149,0
+	.stabs	"wchar_t:t(9,2)=(0,1)",128,0,323,0
+	.stabn	162,0,0,0
+	.stabn	162,0,0,0
+	.stabs	"address_union:T(0,16)=u2word:(3,4),0,16;byte:(0,17)=ar(0,18)=r(0,18);0;0177777;;0;1;(3,2),0,16;;",128,0,0,0
+	.stabs	"length_union:T(0,19)=u2word:(3,4),0,16;byte:(0,17),0,16;;",128,0,0,0
+	.stabs	"flags_struct:T(0,20)=s1eeprom:(0,4),0,1;rampz:(0,4),1,1;;",128,0,0,0
+	.stabs	"putch:F(0,15)",36,0,457,putch
+	.stabs	"ch:P(0,2)",64,0,457,24
+.global	putch
+	.type	putch, @function
+putch:
+	.stabd	46,0,0
+	.stabn	68,0,458,.LM0-.LFBB1
+.LM0:
+.LFBB1:
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
+.L2:
+	.stabn	68,0,460,.LM1-.LFBB1
+.LM1:
+	sbis 43-32,5
+	rjmp .L2
+	.stabn	68,0,461,.LM2-.LFBB1
+.LM2:
+	out 44-32,r24
+/* epilogue start */
+	.stabn	68,0,462,.LM3-.LFBB1
+.LM3:
+	ret
+	.size	putch, .-putch
+.Lscope1:
+	.stabs	"",36,0,0,.Lscope1-.LFBB1
+	.stabd	78,0,0
+	.stabs	"getch:F(0,2)",36,0,464,getch
+.global	getch
+	.type	getch, @function
+getch:
+	.stabd	46,0,0
+	.stabn	68,0,465,.LM4-.LFBB2
+.LM4:
+.LFBB2:
+	push r14
+	push r15
+	push r16
+	push r17
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 4 */
+.L__stack_usage = 4
+	.stabn	68,0,467,.LM5-.LFBB2
+.LM5:
+	clr r14
+	clr r15
+	movw r16,r14
+	.stabn	68,0,468,.LM6-.LFBB2
+.LM6:
+	rjmp .L10
+.L7:
+	.stabn	68,0,470,.LM7-.LFBB2
+.LM7:
+	sec
+	adc r14,__zero_reg__
+	adc r15,__zero_reg__
+	adc r16,__zero_reg__
+	adc r17,__zero_reg__
+	.stabn	68,0,471,.LM8-.LFBB2
+.LM8:
+	ldi r24,lo8(2000001)
+	cp r14,r24
+	ldi r24,hi8(2000001)
+	cpc r15,r24
+	ldi r24,hlo8(2000001)
+	cpc r16,r24
+	ldi r24,hhi8(2000001)
+	cpc r17,r24
+	brlo .L10
+	.stabn	68,0,472,.LM9-.LFBB2
+.LM9:
+	lds r30,app_start
+	lds r31,app_start+1
+	icall
+.L10:
+	.stabn	68,0,468,.LM10-.LFBB2
+.LM10:
+	sbis 43-32,7
+	rjmp .L7
+	.stabn	68,0,474,.LM11-.LFBB2
+.LM11:
+	in r24,44-32
+/* epilogue start */
+	.stabn	68,0,475,.LM12-.LFBB2
+.LM12:
+	pop r17
+	pop r16
+	pop r15
+	pop r14
+	ret
+	.size	getch, .-getch
+	.stabs	"count:r(3,6)",64,0,467,14
+	.stabn	192,0,0,.LFBB2-.LFBB2
+	.stabn	224,0,0,.Lscope2-.LFBB2
+.Lscope2:
+	.stabs	"",36,0,0,.Lscope2-.LFBB2
+	.stabd	78,0,0
+	.stabs	"getNch:F(0,15)",36,0,477,getNch
+	.stabs	"count:P(3,2)",64,0,477,16
+.global	getNch
+	.type	getNch, @function
+getNch:
+	.stabd	46,0,0
+	.stabn	68,0,478,.LM13-.LFBB3
+.LM13:
+.LFBB3:
+	push r16
+	push r17
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 2 */
+.L__stack_usage = 2
+	mov r16,r24
+	.stabn	68,0,480,.LM14-.LFBB3
+.LM14:
+	ldi r17,lo8(0)
+	rjmp .L12
+.L13:
+	.stabn	68,0,484,.LM15-.LFBB3
+.LM15:
+	rcall getch
+	.stabn	68,0,480,.LM16-.LFBB3
+.LM16:
+	subi r17,lo8(-(1))
+.L12:
+	.stabn	68,0,480,.LM17-.LFBB3
+.LM17:
+	cp r17,r16
+	brlo .L13
+/* epilogue start */
+	.stabn	68,0,486,.LM18-.LFBB3
+.LM18:
+	pop r17
+	pop r16
+	ret
+	.size	getNch, .-getNch
+	.stabs	"i:r(3,2)",64,0,479,17
+	.stabn	192,0,0,.LFBB3-.LFBB3
+	.stabn	224,0,0,.Lscope3-.LFBB3
+.Lscope3:
+	.stabs	"",36,0,0,.Lscope3-.LFBB3
+	.stabd	78,0,0
+	.stabs	"byte_response:F(0,15)",36,0,488,byte_response
+	.stabs	"val:P(3,2)",64,0,488,17
+.global	byte_response
+	.type	byte_response, @function
+byte_response:
+	.stabd	46,0,0
+	.stabn	68,0,489,.LM19-.LFBB4
+.LM19:
+.LFBB4:
+	push r17
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 1 */
+.L__stack_usage = 1
+	mov r17,r24
+	.stabn	68,0,490,.LM20-.LFBB4
+.LM20:
+	rcall getch
+	cpi r24,lo8(32)
+	brne .L14
+	.stabn	68,0,491,.LM21-.LFBB4
+.LM21:
+	ldi r24,lo8(20)
+	rcall putch
+	.stabn	68,0,492,.LM22-.LFBB4
+.LM22:
+	mov r24,r17
+	rcall putch
+	.stabn	68,0,493,.LM23-.LFBB4
+.LM23:
+	ldi r24,lo8(16)
+	rcall putch
+.L14:
+/* epilogue start */
+	.stabn	68,0,495,.LM24-.LFBB4
+.LM24:
+	pop r17
+	ret
+	.size	byte_response, .-byte_response
+.Lscope4:
+	.stabs	"",36,0,0,.Lscope4-.LFBB4
+	.stabd	78,0,0
+	.stabs	"nothing_response:F(0,15)",36,0,497,nothing_response
+.global	nothing_response
+	.type	nothing_response, @function
+nothing_response:
+	.stabd	46,0,0
+	.stabn	68,0,498,.LM25-.LFBB5
+.LM25:
+.LFBB5:
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
+	.stabn	68,0,499,.LM26-.LFBB5
+.LM26:
+	rcall getch
+	cpi r24,lo8(32)
+	brne .L16
+	.stabn	68,0,500,.LM27-.LFBB5
+.LM27:
+	ldi r24,lo8(20)
+	rcall putch
+	.stabn	68,0,501,.LM28-.LFBB5
+.LM28:
+	ldi r24,lo8(16)
+	rcall putch
+.L16:
+	ret
+	.size	nothing_response, .-nothing_response
+.Lscope5:
+	.stabs	"",36,0,0,.Lscope5-.LFBB5
+	.stabd	78,0,0
+	.stabs	"main:F(0,1)",36,0,129,main
+.global	main
+	.type	main, @function
+main:
+	.stabd	46,0,0
+	.stabn	68,0,130,.LM29-.LFBB6
+.LM29:
+.LFBB6:
+	push r12
+	push r13
+	push r14
+	push r15
+	push r16
+	push r17
+	push r28
+	push r29
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 8 */
+.L__stack_usage = 8
+	.stabn	68,0,137,.LM30-.LFBB6
+.LM30:
+/* #APP */
+ ;  137 "ATmegaBOOT.c" 1
+	nop
+	
+ ;  0 "" 2
+	.stabn	68,0,148,.LM31-.LFBB6
+.LM31:
+/* #NOAPP */
+	out 64-32,__zero_reg__
+	.stabn	68,0,149,.LM32-.LFBB6
+.LM32:
+	ldi r24,lo8(12)
+	out 41-32,r24
+	.stabn	68,0,150,.LM33-.LFBB6
+.LM33:
+	ldi r24,lo8(24)
+	out 42-32,r24
+	.stabn	68,0,151,.LM34-.LFBB6
+.LM34:
+	ldi r24,lo8(-122)
+	out 64-32,r24
+	.stabn	68,0,169,.LM35-.LFBB6
+.LM35:
+	sbi 55-32,5
+	.stabn	68,0,170,.LM36-.LFBB6
+.LM36:
+	sts i,__zero_reg__
+	.stabn	68,0,171,.LM37-.LFBB6
+.LM37:
+	ldi r21,lo8(32)
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/util/delay_basic.h",132,0,0,.Ltext1
+.Ltext1:
+	.stabn	68,0,105,.LM38-.LFBB6
+.LM38:
+	ldi r18,lo8(0)
+	ldi r19,hi8(0)
+	.stabs	"ATmegaBOOT.c",132,0,0,.Ltext2
+.Ltext2:
+	.stabn	68,0,170,.LM39-.LFBB6
+.LM39:
+	rjmp .L19
+.L20:
+	.stabn	68,0,171,.LM40-.LFBB6
+.LM40:
+	in r24,56-32
+	eor r24,r21
+	out 56-32,r24
+	.stabs	"/usr/lib/gcc/avr/4.5.3/../../../avr/include/util/delay_basic.h",132,0,0,.Ltext3
+.Ltext3:
+	.stabn	68,0,105,.LM41-.LFBB6
+.LM41:
+	movw r24,r18
+/* #APP */
+ ;  105 "/usr/lib/gcc/avr/4.5.3/../../../avr/include/util/delay_basic.h" 1
+	1: sbiw r24,1
+	brne 1b
+ ;  0 "" 2
+	.stabs	"ATmegaBOOT.c",132,0,0,.Ltext4
+.Ltext4:
+	.stabn	68,0,170,.LM42-.LFBB6
+.LM42:
+/* #NOAPP */
+	subi r20,lo8(-(1))
+	sts i,r20
+.L19:
+	.stabn	68,0,170,.LM43-.LFBB6
+.LM43:
+	lds r20,i
+	cpi r20,lo8(16)
+	brlo .L20
+.L60:
+	.stabn	68,0,193,.LM44-.LFBB6
+.LM44:
+	rcall getch
+	.stabn	68,0,198,.LM45-.LFBB6
+.LM45:
+	cpi r24,lo8(48)
+	breq .L64
+.L22:
+	.stabn	68,0,205,.LM46-.LFBB6
+.LM46:
+	cpi r24,lo8(49)
+	brne .L24
+	.stabn	68,0,206,.LM47-.LFBB6
+.LM47:
+	rcall getch
+	cpi r24,lo8(32)
+	brne .L60
+	.stabn	68,0,207,.LM48-.LFBB6
+.LM48:
+	ldi r24,lo8(20)
+	rcall putch
+	.stabn	68,0,208,.LM49-.LFBB6
+.LM49:
+	ldi r24,lo8(65)
+	rcall putch
+	.stabn	68,0,209,.LM50-.LFBB6
+.LM50:
+	ldi r24,lo8(86)
+	rcall putch
+	.stabn	68,0,210,.LM51-.LFBB6
+.LM51:
+	ldi r24,lo8(82)
+	rcall putch
+	.stabn	68,0,211,.LM52-.LFBB6
+.LM52:
+	ldi r24,lo8(32)
+	rcall putch
+	.stabn	68,0,212,.LM53-.LFBB6
+.LM53:
+	ldi r24,lo8(73)
+	rcall putch
+	.stabn	68,0,213,.LM54-.LFBB6
+.LM54:
+	ldi r24,lo8(83)
+	rcall putch
+	.stabn	68,0,214,.LM55-.LFBB6
+.LM55:
+	ldi r24,lo8(80)
+	rjmp .L62
+.L24:
+	.stabn	68,0,220,.LM56-.LFBB6
+.LM56:
+	cpi r24,lo8(64)
+	brne .L25
+	.stabn	68,0,221,.LM57-.LFBB6
+.LM57:
+	rcall getch
+	.stabn	68,0,222,.LM58-.LFBB6
+.LM58:
+	cpi r24,lo8(-122)
+	brlo .L64
+	.stabn	68,0,222,.LM59-.LFBB6
+.LM59:
+	rcall getch
+	rjmp .L64
+.L25:
+	.stabn	68,0,227,.LM60-.LFBB6
+.LM60:
+	cpi r24,lo8(65)
+	brne .L27
+	.stabn	68,0,228,.LM61-.LFBB6
+.LM61:
+	rcall getch
+	.stabn	68,0,229,.LM62-.LFBB6
+.LM62:
+	cpi r24,lo8(-128)
+	brne .L28
+	.stabn	68,0,229,.LM63-.LFBB6
+.LM63:
+	ldi r24,lo8(2)
+	rjmp .L65
+.L28:
+	.stabn	68,0,230,.LM64-.LFBB6
+.LM64:
+	cpi r24,lo8(-127)
+	brne .L29
+	.stabn	68,0,230,.LM65-.LFBB6
+.LM65:
+	ldi r24,lo8(1)
+	rjmp .L65
+.L29:
+	.stabn	68,0,231,.LM66-.LFBB6
+.LM66:
+	cpi r24,lo8(-126)
+	breq .+2
+	rjmp .L67
+	.stabn	68,0,231,.LM67-.LFBB6
+.LM67:
+	ldi r24,lo8(18)
+	rjmp .L65
+.L27:
+	.stabn	68,0,237,.LM68-.LFBB6
+.LM68:
+	cpi r24,lo8(66)
+	brne .L31
+	.stabn	68,0,238,.LM69-.LFBB6
+.LM69:
+	ldi r24,lo8(20)
+.L66:
+	rcall getNch
+.L64:
+	.stabn	68,0,239,.LM70-.LFBB6
+.LM70:
+	rcall nothing_response
+	rjmp .L60
+.L31:
+	.stabn	68,0,243,.LM71-.LFBB6
+.LM71:
+	cpi r24,lo8(69)
+	brne .L32
+	.stabn	68,0,244,.LM72-.LFBB6
+.LM72:
+	ldi r24,lo8(5)
+	rjmp .L66
+.L32:
+	.stabn	68,0,249,.LM73-.LFBB6
+.LM73:
+	cpi r24,lo8(80)
+	breq .L64
+.L33:
+	.stabn	68,0,256,.LM74-.LFBB6
+.LM74:
+	cpi r24,lo8(81)
+	breq .L64
+.L34:
+	.stabn	68,0,266,.LM75-.LFBB6
+.LM75:
+	cpi r24,lo8(82)
+	breq .L64
+.L35:
+	.stabn	68,0,273,.LM76-.LFBB6
+.LM76:
+	cpi r24,lo8(85)
+	brne .L36
+	.stabn	68,0,274,.LM77-.LFBB6
+.LM77:
+	rcall getch
+	sts address,r24
+	.stabn	68,0,275,.LM78-.LFBB6
+.LM78:
+	rcall getch
+	sts address+1,r24
+	rjmp .L64
+.L36:
+	.stabn	68,0,280,.LM79-.LFBB6
+.LM79:
+	cpi r24,lo8(86)
+	brne .L37
+	.stabn	68,0,281,.LM80-.LFBB6
+.LM80:
+	ldi r24,lo8(4)
+	rcall getNch
+	rjmp .L67
+.L37:
+	.stabn	68,0,286,.LM81-.LFBB6
+.LM81:
+	cpi r24,lo8(100)
+	breq .+2
+	rjmp .L38
+	.stabn	68,0,287,.LM82-.LFBB6
+.LM82:
+	rcall getch
+	sts length+1,r24
+	.stabn	68,0,288,.LM83-.LFBB6
+.LM83:
+	rcall getch
+	sts length,r24
+	.stabn	68,0,289,.LM84-.LFBB6
+.LM84:
+	lds r24,flags
+	andi r24,lo8(-2)
+	sts flags,r24
+	.stabn	68,0,290,.LM85-.LFBB6
+.LM85:
+	rcall getch
+	cpi r24,lo8(69)
+	brne .L39
+	.stabn	68,0,290,.LM86-.LFBB6
+.LM86:
+	lds r24,flags
+	ori r24,lo8(1)
+	sts flags,r24
+.L39:
+	ldi r19,lo8(buff)
+	mov r12,r19
+	ldi r19,hi8(buff)
+	mov r13,r19
+	.stabn	68,0,130,.LM87-.LFBB6
+.LM87:
+	movw r14,r12
+	ldi r16,lo8(0)
+	ldi r17,hi8(0)
+	rjmp .L40
+.L41:
+	.stabn	68,0,292,.LM88-.LFBB6
+.LM88:
+	rcall getch
+	movw r30,r14
+	st Z+,r24
+	movw r14,r30
+	.stabn	68,0,291,.LM89-.LFBB6
+.LM89:
+	subi r16,lo8(-(1))
+	sbci r17,hi8(-(1))
+.L40:
+	.stabn	68,0,291,.LM90-.LFBB6
+.LM90:
+	lds r24,length
+	lds r25,length+1
+	cp r16,r24
+	cpc r17,r25
+	brlo .L41
+	.stabn	68,0,294,.LM91-.LFBB6
+.LM91:
+	rcall getch
+	cpi r24,lo8(32)
+	breq .+2
+	rjmp .L60
+	.stabn	68,0,295,.LM92-.LFBB6
+.LM92:
+	lds r24,flags
+	sbrc r24,0
+	rjmp .L56
+	rjmp .L69
+.L44:
+	.stabn	68,0,297,.LM93-.LFBB6
+.LM93:
+	movw r30,r12
+	ld r22,Z+
+	movw r12,r30
+	lds r24,address
+	lds r25,address+1
+	rcall __eewr_byte_m8
+	.stabn	68,0,298,.LM94-.LFBB6
+.LM94:
+	lds r24,address
+	lds r25,address+1
+	adiw r24,1
+	sts address+1,r25
+	sts address,r24
+	.stabn	68,0,296,.LM95-.LFBB6
+.LM95:
+	subi r16,lo8(-(1))
+	sbci r17,hi8(-(1))
+	rjmp .L42
+.L56:
+	.stabn	68,0,295,.LM96-.LFBB6
+.LM96:
+	ldi r16,lo8(0)
+	ldi r17,hi8(0)
+.L42:
+	.stabn	68,0,296,.LM97-.LFBB6
+.LM97:
+	lds r24,length
+	lds r25,length+1
+	cp r16,r24
+	cpc r17,r25
+	brlo .L44
+	rjmp .L45
+.L69:
+	.stabn	68,0,306,.LM98-.LFBB6
+.LM98:
+/* #APP */
+ ;  306 "ATmegaBOOT.c" 1
+	cli
+ ;  0 "" 2
+/* #NOAPP */
+.L46:
+	.stabn	68,0,307,.LM99-.LFBB6
+.LM99:
+	sbic 60-32,1
+	rjmp .L46
+	.stabn	68,0,308,.LM100-.LFBB6
+.LM100:
+/* #APP */
+ ;  308 "ATmegaBOOT.c" 1
+	clr	r17		
+	lds	r30,address	
+	lds	r31,address+1	
+	lsl r30				
+	rol r31				
+	ldi	r28,lo8(buff)	
+	ldi	r29,hi8(buff)	
+	lds	r24,length	
+	lds	r25,length+1	
+	sbrs r24,0		
+	rjmp length_loop		
+	adiw r24,1		
+	length_loop:		
+	cpi	r17,0x00	
+	brne	no_page_erase	
+	rcall  wait_spm		
+	ldi	r16,0x03	
+	sts	87,r16		
+	spm			
+	rcall  wait_spm		
+	ldi	r16,0x11	
+	sts	87,r16		
+	spm			
+	no_page_erase:		
+	ld	r0,Y+		
+	ld	r1,Y+		
+	rcall  wait_spm		
+	ldi	r16,0x01	
+	sts	87,r16		
+	spm			
+	inc	r17		
+	cpi r17,32	        
+	brlo	same_page	
+	write_page:		
+	clr	r17		
+	rcall  wait_spm		
+	ldi	r16,0x05	
+	sts	87,r16		
+	spm			
+	rcall  wait_spm		
+	ldi	r16,0x11	
+	sts	87,r16		
+	spm			
+	same_page:		
+	adiw	r30,2		
+	sbiw	r24,2		
+	breq	final_write	
+	rjmp	length_loop	
+	wait_spm:  
+	lds	r16,87		
+	andi	r16,1           
+	cpi	r16,1           
+	breq	wait_spm       
+	ret			
+	final_write:		
+	cpi	r17,0		
+	breq	block_done	
+	adiw	r24,2		
+	rjmp	write_page	
+	block_done:		
+	clr	__zero_reg__	
+	
+ ;  0 "" 2
+/* #NOAPP */
+.L45:
+	.stabn	68,0,404,.LM101-.LFBB6
+.LM101:
+	ldi r24,lo8(20)
+	rjmp .L62
+.L38:
+	.stabn	68,0,410,.LM102-.LFBB6
+.LM102:
+	cpi r24,lo8(116)
+	breq .+2
+	rjmp .L47
+	.stabn	68,0,411,.LM103-.LFBB6
+.LM103:
+	rcall getch
+	sts length+1,r24
+	.stabn	68,0,412,.LM104-.LFBB6
+.LM104:
+	rcall getch
+	sts length,r24
+	.stabn	68,0,413,.LM105-.LFBB6
+.LM105:
+	rcall getch
+	lds r25,flags
+	cpi r24,lo8(69)
+	brne .L48
+	.stabn	68,0,413,.LM106-.LFBB6
+.LM106:
+	ori r25,lo8(1)
+	sts flags,r25
+	rjmp .L49
+.L48:
+	.stabn	68,0,415,.LM107-.LFBB6
+.LM107:
+	andi r25,lo8(-2)
+	sts flags,r25
+	.stabn	68,0,416,.LM108-.LFBB6
+.LM108:
+	lds r24,address
+	lds r25,address+1
+	lsl r24
+	rol r25
+	sts address+1,r25
+	sts address,r24
+.L49:
+	.stabn	68,0,418,.LM109-.LFBB6
+.LM109:
+	rcall getch
+	cpi r24,lo8(32)
+	breq .+2
+	rjmp .L60
+	.stabn	68,0,419,.LM110-.LFBB6
+.LM110:
+	ldi r24,lo8(20)
+	rcall putch
+	.stabn	68,0,420,.LM111-.LFBB6
+.LM111:
+	ldi r16,lo8(0)
+	ldi r17,hi8(0)
+	rjmp .L50
+.L54:
+	.stabn	68,0,421,.LM112-.LFBB6
+.LM112:
+	lds r24,flags
+	sbrs r24,0
+	rjmp .L51
+	.stabn	68,0,422,.LM113-.LFBB6
+.LM113:
+	lds r24,address
+	lds r25,address+1
+	rcall __eerd_byte_m8
+	rjmp .L68
+.L51:
+	.stabn	68,0,425,.LM114-.LFBB6
+.LM114:
+	sbrc r24,1
+	rjmp .L53
+	.stabn	68,0,425,.LM115-.LFBB6
+.LM115:
+	lds r30,address
+	lds r31,address+1
+/* #APP */
+ ;  425 "ATmegaBOOT.c" 1
+	lpm r24, Z
+	
+ ;  0 "" 2
+/* #NOAPP */
+.L68:
+	rcall putch
+.L53:
+	.stabn	68,0,426,.LM116-.LFBB6
+.LM116:
+	lds r24,address
+	lds r25,address+1
+	adiw r24,1
+	sts address+1,r25
+	sts address,r24
+	.stabn	68,0,420,.LM117-.LFBB6
+.LM117:
+	subi r16,lo8(-(1))
+	sbci r17,hi8(-(1))
+.L50:
+	.stabn	68,0,420,.LM118-.LFBB6
+.LM118:
+	lds r24,length
+	lds r25,length+1
+	cp r16,r24
+	cpc r17,r25
+	brlo .L54
+	rjmp .L63
+.L47:
+	.stabn	68,0,434,.LM119-.LFBB6
+.LM119:
+	cpi r24,lo8(117)
+	brne .L55
+	.stabn	68,0,435,.LM120-.LFBB6
+.LM120:
+	rcall getch
+	cpi r24,lo8(32)
+	breq .+2
+	rjmp .L60
+	.stabn	68,0,436,.LM121-.LFBB6
+.LM121:
+	ldi r24,lo8(20)
+	rcall putch
+	.stabn	68,0,437,.LM122-.LFBB6
+.LM122:
+	ldi r24,lo8(30)
+	rcall putch
+	.stabn	68,0,438,.LM123-.LFBB6
+.LM123:
+	ldi r24,lo8(-109)
+	rcall putch
+	.stabn	68,0,439,.LM124-.LFBB6
+.LM124:
+	ldi r24,lo8(7)
+.L62:
+	rcall putch
+.L63:
+	.stabn	68,0,440,.LM125-.LFBB6
+.LM125:
+	ldi r24,lo8(16)
+	rcall putch
+	rjmp .L60
+.L55:
+	.stabn	68,0,445,.LM126-.LFBB6
+.LM126:
+	cpi r24,lo8(118)
+	breq .+2
+	rjmp .L60
+.L67:
+	.stabn	68,0,446,.LM127-.LFBB6
+.LM127:
+	ldi r24,lo8(0)
+.L65:
+	rcall byte_response
+	rjmp .L60
+	.size	main, .-main
+	.stabs	"ch:r(3,2)",64,0,131,24
+	.stabn	192,0,0,.LFBB6-.LFBB6
+	.stabn	224,0,0,.Lscope6-.LFBB6
+.Lscope6:
+	.stabs	"",36,0,0,.Lscope6-.LFBB6
+	.stabd	78,0,0
+.global	pagesz
+	.data
+	.type	pagesz, @object
+	.size	pagesz, 1
+pagesz:
+	.byte	-128
+.global	app_start
+.global	app_start
+	.section .bss
+	.type	app_start, @object
+	.size	app_start, 2
+app_start:
+	.skip 2,0
+	.comm address,2,1
+	.comm length,2,1
+	.comm flags,1,1
+	.comm buff,256,1
+	.comm i,1,1
+	.stabs	"address:G(0,16)",32,0,106,0
+	.stabs	"length:G(0,19)",32,0,111,0
+	.stabs	"flags:G(0,20)",32,0,116,0
+	.stabs	"buff:G(0,21)=ar(0,18);0;255;(3,2)",32,0,118,0
+	.stabs	"pagesz:G(3,2)",32,0,121,0
+	.stabs	"i:G(3,2)",32,0,123,0
+	.stabs	"app_start:G(0,22)=*(0,23)=f(0,15)",32,0,127,0
+	.text
+	.stabs	"",100,0,0,.Letext0
+.Letext0:
+.global __do_copy_data
+.global __do_clear_bss
